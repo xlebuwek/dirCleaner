63b68cb794fb0739e97e0042b26c8d5739e07204
xlebuwek <66259776+xlebuwek@users.noreply.github.com>
2021-05-05 18:36:09 +0500

    v0.1
    first version (many bugs)

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from directory import Directory
 from window import MainWindow
 import os
-dir = Directory(MainWindow.Create_inputbox('1', '1', 'Отправить')).getdirectory()
-types = []
-os.chdir(dir)
-for i in os.listdir():
+
+def getfiletype(name):
     type = ""
-    f = False #эта штука проверяет, была ли точка до текущей j
+    f = False  # эта штука проверяет, была ли точка до текущей j
     for j in i:
         if j == '.':
             f = True
         if f:
-            type+=j
-    if type not in types and type != "":
-        types.append(type)
+            type += j
+    return type
+
+dir = Directory(MainWindow.Create_inputbox('1', '1', 'Отправить')).getdirectory()
+types = []
+os.chdir(dir)
+for i in os.listdir():
+    if getfiletype(i) not in types and getfiletype(i) != "":
+        types.append(getfiletype(i))
 for i in types:
     os.mkdir(i)
-print(os.listdir())
-#for i in os.listdir():
- #   os.replace(dir+"\\"+i, )
+
+os.chdir(dir)
+
+for i in os.listdir():
+    if getfiletype(i) != "" and i not in types:
+        os.replace(dir+"\\"+i, dir+"\\"+getfiletype(i)+"\\"+i)
+        print(dir+"\\"+i, dir+"\\"+getfiletype(i)+"\\"+i)
